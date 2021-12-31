8f60137d683d9b77489c5334e5249cefe7cb93f5
shelle <mohammed.shelle@fcmb.com>
2022-01-01 00:18:49 +0100

    migrate table to azure

---
+++ LandReal/Server/Migrations/20211231230534_initialDB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LandReal.Server.Migrations
+{
+    public partial class initialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogDate = table.Column<DateTime>(type: "datetime2", nullable: false,defaultValueSql:"GetDate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    LogDate = table.Column<DateTime>(type: "datetime2", nullable: false,defaultValueSql:"GetDate()"),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "PayRole" },
+                    { 4, "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "Amount", "DateOfBirth", "DepartmentId", "Email", "FirstName", "Gender", "IsActive", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, 23.449999999999999, new DateTime(1980, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "h@h.com", "John", 0, true, "Hastings", "images/bab.jpg" },
+                    { 2, 93.450000000000003, new DateTime(1988, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "a@a.com", "Abe", 1, false, "Olufunmi", "images/oab.jpg" },
+                    { 3, 83.450000000000003, new DateTime(1989, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "o@h.com", "Ola", 0, true, "lofet", "images/yab.jpg" },
+                    { 4, 66.450000000000003, new DateTime(1990, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, "p@h.com", "Laide", 0, false, "kings", "images/bab.jpg" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
